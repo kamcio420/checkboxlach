90a21e5a43c252df82b23d6c00a7e9805b2bb078
kamcio420 <kamcio420@gmail.com>
2022-03-03 12:55:38 +0100

    2

--- app/src/main/java/com/example/checkboxlach/MainActivity.kt
+++ app/src/main/java/com/example/checkboxlach/MainActivity.kt
@@ -6,32 +6,32 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.StringBuilder
 class MainActivity : AppCompatActivity() {
-    lateinit var pizza: CheckBox
-    lateinit var coffee: CheckBox
-    lateinit var burger: CheckBox
+    lateinit var rollo: CheckBox
+    lateinit var soup: CheckBox
+    lateinit var burgier: CheckBox
     lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "KotlinApp"
-        pizza = findViewById(R.id.checkBox1)
-        coffee = findViewById(R.id.checkBox2)
-        burger = findViewById(R.id.checkBox3)
+        rollo = findViewById(R.id.checkBox1)
+        soup = findViewById(R.id.checkBox2)
+        burgier = findViewById(R.id.checkBox3)
         button = findViewById(R.id.button)
         button.setOnClickListener {
             var totalAmount: Int = 0
             val result = StringBuilder()
             result.append("Selected Items")
-            if (pizza.isChecked) {
-                result.append("\nBurgier 8USD")
+            if (rollo.isChecked) {
+                result.append("\nRollo 7USD")
                 totalAmount += 100
             }
-            if (coffee.isChecked) {
-                result.append("\nCoffee 50Rs")
+            if (soup.isChecked) {
+                result.append("\nTomato soup 4USD")
                 totalAmount += 50
             }
-            if (burger.isChecked) {
-                result.append("\nBurger 120Rs")
+            if (burgier.isChecked) {
+                result.append("\nBurgier 8USD")
                 totalAmount += 120
             }
             result.append("\nTotal: " + totalAmount + "Rs")
